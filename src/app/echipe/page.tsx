bd6eca67e1c365f65878792ebd3a0b324c3716ba
Andrei Cruceru <andreicruceru@MacBook-Air-Andrei.local>
2025-02-28 15:31:50 +0200

    same padding

--- src/app/echipe/page.tsx
+++ src/app/echipe/page.tsx
@@ -13,7 +13,7 @@ export const dynamic = 'force-dynamic';
 
 export default function TeamsPage() {
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-2 sm:p-4">
       <div className="mb-8 text-center">
         <h1 className="text-3xl font-bold mb-4 inline-flex items-center justify-center">
           <span className="bg-green-600 text-white px-4 py-2 rounded-lg">
